0e4d4f30fd5979c6ae6ebb531fc7cff579596f2f
hfriedrich <heiko.friedrich@researchstudio.at>
2014-07-22 16:13:02 +0200

    add connection prediction

--- src/main/python/rescalprocessing.py
+++ src/main/python/rescalprocessing.py
@@ -16,7 +16,6 @@ from sklearn.metrics import precision_recall_curve, auc
 from rescal import rescal_als
 from sklearn import datasets
 
-
 def predict_rescal_als(T, rank):
     A, R, _, _, _ = rescal_als(
         T, rank, init='nvecs', conv=1e-3,
@@ -34,20 +33,16 @@ def normalize_predictions(P, e, k):
         for b in range(e):
             nrm = norm(P[a, b, :k])
             if nrm != 0:
-                # round values for faster computation of AUC-PR
                 P[a, b, :k] = np.round_(P[a, b, :k] / nrm, decimals=3)
     return P
 
+def read_input_tensor(data_file, header_file):
+    _log.info("Read mat input file: " + data_file)
+    mat = loadmat(data_file)
 
-if __name__ == '__main__':
-
-    # load data
-    _log.info("Read mat input file: " + sys.argv[1])
-    mat = loadmat(sys.argv[1])
-
-    _log.info("Read header input file: " + sys.argv[2])
-    input = open(sys.argv[2])
-    entities = input.read().splitlines()
+    _log.info("Read header input file: " + header_file)
+    input = open(header_file)
+    headers = input.read().splitlines()
 
     K = []
     for i in range(0,3):
@@ -56,49 +51,57 @@ if __name__ == '__main__':
         V = G.data
         K.append(sparse.csr_matrix((V,(I,J)),G.shape))
 
-    rank = 100
-    e = K[0].shape[0]
-    k = 1
-    SZ = e * e * k;
+    return K, headers
 
-    _log.info('start processing ...')
-    _log.info('Datasize: %d x %d x %d | No. of classes: %d | Rank: %d' % (
-        K[0].shape + (len(K),) + (k,) + (rank,))
-    )
-
-    # rescal processing
-    P = predict_rescal_als(K, rank)
-
-    #_log.info('normalize predictions ...')
-    #P = normalize_predictions(P, e, k)
-
-    _log.info('Writing output file: ' + sys.argv[3])
-    out = open(sys.argv[3], 'w+')
+def write_term_output(file, P, slice, entities):
+    _log.info('Writing output file: ' + file)
+    out = open(file, 'w+')
     for line in range(0, len(entities)):
-        darr = np.array(P[line,:,2])
+        darr = np.array(P[line,:,slice])
         indices = (np.argsort(darr))[-20:]
         predicted_entities = [entities[i][6:] + " (" + str(round(darr[i], 2)) + ")" for i in reversed(indices)]
         need = entities[line].ljust(150)
         if (need.startswith('Need:')):
             out.write(need + ': ' + ', '.join(predicted_entities) + '\n')
 
-    _log.info('Writing output file: ' + sys.argv[3] + ".con")
-    out = open(sys.argv[3] + ".con", 'w+')
+def write_connection_output(file, P, slice, entities):
+    _log.info('Writing output file: ' + file)
+    out = open(file, 'w+')
     for line in range(0, len(entities)):
-        darr = np.array(P[line,:,0])
-        indices = (np.argsort(darr))[-20:]
-        if darr[indices[-1]] > 0.1:
-            newIndices = []
-            for i in indices:
-                if darr[i] > 0.1:
-                    newIndices.append(i)
-            indices = newIndices
-            predicted_entities = [entities[i] + " (" + str(round(darr[i], 2)) + ")" for i in reversed(indices)]
-            need = entities[line].ljust(150)
-            if (need.startswith('Need:')):
-                out.write(need[6:] + '\n')
+        if (entities[line].startswith('Need:')):
+            darr = np.array(P[line,:,slice])
+            indices = reversed((np.argsort(darr))[-20:])
+            indices = [i for i in indices if darr[i] > 0.1]
+            if len(indices) > 0:
+                predicted_entities = [entities[i] + " (" + str(round(darr[i], 2)) + ")" for i in indices if entities[
+                    i].startswith('Need:')]
+                out.write(entities[line][6:] + '\n')
                 for entity in predicted_entities:
-                    if (entity.startswith('Need:')):
-                        out.write(entity[6:] + '\n')
-            out.write('\n')
+                    out.write(entity[6:] + '\n')
+                out.write('\n')
+
+
+if __name__ == '__main__':
+
+    # load the tensor input data
+    K, entities = read_input_tensor(sys.argv[1], sys.argv[2])
+
+    rank = 100
+    _log.info('start processing ...')
+    _log.info('Datasize: %d x %d x %d | Rank: %d' % (
+        K[0].shape + (len(K),) + (rank,))
+    )
+
+    # execute rescal algorithm
+    P = predict_rescal_als(K, rank)
+
+    # topic attributes (terms) are saved in slice 2
+    write_term_output(sys.argv[3] + "/outterm.txt", P, 2, entities)
+
+    # connections are saved in slice 0
+    write_connection_output(sys.argv[3] + "/outconn.txt", P, 0, entities)
+
+
+
+
 
