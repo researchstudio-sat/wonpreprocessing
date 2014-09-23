5de9da71a9dade0c74a842b766ea206f5fec9036
yanchith <yanchi.toth@gmail.com>
2014-09-23 14:32:32 +0200

    add keyword slice progress

---
+++ python-processing/src/main/python/add_keyword_slice.py
@@ -0,0 +1,69 @@
+import sys
+from os import listdir
+from os.path import isfile, join
+
+import numpy as np
+from scipy.sparse.coo import coo_matrix
+from scipy.io import mmwrite
+
+from feature_extraction import vectorize_and_transform, apply_threshold
+from evaluate_link_prediction import read_input_tensor
+
+
+if len(sys.argv) != 3:
+    raise Exception("ARGUMENTS: <documents dir> <rescal_dir>")
+
+doc_path = sys.argv[1]
+rescal_path = sys.argv[2]
+
+documents = [f.rstrip('.eml') for f in listdir(doc_path) if
+             isfile(join(doc_path, f)) and f.endswith('.eml')]
+print('Loaded ', len(documents), ' files from path: ', doc_path)
+
+print('Extracting features')
+
+file_paths = [join(doc_path, f + '.eml') for f in documents]
+data, features = vectorize_and_transform(file_paths)
+data = apply_threshold(data, 0.3)
+
+# TODO: filter features and filenames based on whether their entries survived
+
+print('Reading tensor')
+tensor, headers = read_input_tensor(rescal_path)
+
+NEED_STR = 'Need: '
+ATTR_STR = 'Attr: '
+LEN = len(NEED_STR)
+
+document_index = {}
+headers_cursor = 1
+for header in headers:
+    if header.startswith(NEED_STR):
+        document_index[header[LEN:]] = headers_cursor
+    headers_cursor += 1
+
+for filename in documents:
+    if filename not in document_index:
+        # print('Adding NEED at index (', headers_cursor, '): ', filename)
+        headers.append(NEED_STR + filename)
+        document_index[filename] = headers_cursor
+        headers_cursor += 1
+
+feature_index = {}
+for feature in features:
+    # print('Adding ATTR at index (', headers_cursor, '): ', feature)
+    headers.append(ATTR_STR + feature)
+    feature_index[feature] = headers_cursor
+    headers_cursor += 1
+
+offset_row = np.array([document_index[documents[i]] for i in data.row])
+offset_col = np.array([feature_index[features[i]] for i in data.col])
+
+offset_matrix = coo_matrix((data.data, (offset_row, offset_col)))
+
+with open(rescal_path + '/headers.txt', mode='w', encoding='utf-8') as f:
+    f.write('\n'.join(headers))
+
+mmwrite(rescal_path + '/keywords_slice.mtx', offset_matrix)
+
+
--- python-processing/src/main/python/feature_extraction.py
+++ python-processing/src/main/python/feature_extraction.py
@@ -1,99 +1,95 @@
-import sys
-from os import listdir
-from os.path import isfile, join
-
 import numpy as np
+from scipy.sparse.coo import coo_matrix
+
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 import nltk
 from nltk.stem import WordNetLemmatizer
 
 
-class PosTagLemmaTokenizer(object):
+_wnl = WordNetLemmatizer()
+
+_filter_threshold = np.vectorize(lambda x, threshold: x if x > threshold else 0,
+                                 excluded={'threshold'})
+
+
+def _clean_mail(doc):
+    """Return only the Subject and Content parts from the mail."""
+    subject, content = False, False
+    accepted_lines = []
+    for line in doc.splitlines(keepends=True):
+        l = line.lower()
+        if l.startswith('subject'):
+            subject, content = True, False
+            line = line.lstrip('subject')
+        elif l.startswith('content'):
+            subject, content = False, True
+            line = line.lstrip('content')
+        if subject or content:
+            accepted_lines.append(line.strip())
+    return ''.join(accepted_lines)
+
+
+def lemma_tokenizer(doc):
+    return [_wnl.lemmatize(t) for t in nltk.word_tokenize(_clean_mail(doc))]
+
+
+class PosTagLemmaTokenizer:
     def __init__(self):
         self.tags = {'NN', 'NNS', 'VBZ', 'JJ', 'RB', 'VBG'}
         self.tagger = nltk.data.load(nltk.tag._POS_TAGGER)
-        self.wnl = WordNetLemmatizer()
 
     def __call__(self, doc):
-        subject, content = False, False
-        accepted_lines = []
-        for line in doc.splitlines(keepends=True):
-            l = line.lower()
-            if l.startswith('subject'):
-                subject = True
-                line = line.lstrip('subject')
-            elif l.startswith('content'):
-                subject, content = False, True
-                line = line.lstrip('content')
-            if subject or content:
-                accepted_lines.append(line.strip())
-        doc = ''.join(accepted_lines)
-        tokens = nltk.word_tokenize(doc)
-        # return [self.wnl.lemmatize(t) for t in tokens]
+        tokens = nltk.word_tokenize(_clean_mail(doc))
         tagged = self.tagger.tag(tokens)
-        return [self.wnl.lemmatize(t[0]) for t in tagged if t[1] in self.tags]
-
-vectorizer = TfidfVectorizer(min_df=2, stop_words='english', input='filename',
-                             ngram_range=(1, 2), tokenizer=PosTagLemmaTokenizer())
+        return [_wnl.lemmatize(t[0]) for t in tagged if t[1] in self.tags]
 
 
-def vectorize_and_filter(filenames, threshold):
+def vectorize_and_transform(filenames, tokenizer=None):
+    if tokenizer is None:
+        tokenizer = lemma_tokenizer
+
+    vectorizer = TfidfVectorizer(min_df=2, stop_words='english',
+                                 input='filename', ngram_range=(1, 2),
+                                 tokenizer=tokenizer)
     fit = vectorizer.fit_transform(filenames)
-    return fit, np.nonzero(fit > threshold)
+    return fit, vectorizer.get_feature_names()
 
 
+def apply_threshold(original, threshold):
+    original = coo_matrix(original)  # Ensure COO format
+    indices = original.data > threshold
+    new_data = original.data[indices]
+    new_matrix = new_data, (original.row[indices], original.col[indices])
+    return coo_matrix(new_matrix)
+
+# Run an example
 if __name__ == '__main__':
+    import sys
+    from os import listdir
+    from os.path import isfile, join
+
     if len(sys.argv) != 2:
         raise Exception("USAGE: python feature_extraction.py <input dir>")
 
     path = sys.argv[1]  # 0-th argument is the script name.
-    print('Loading corpus documents from ', path)
-
     files = [join(path, f) for f in listdir(path) if
              isfile(join(path, f)) and f.endswith('.eml')]
 
-    print('Loaded ', len(files), ' files')
+    print('Loaded ', len(files), ' files from path: ', path)
 
-    data, above_threshold = vectorize_and_filter(files, 0.3)
-    print(data)
-    print()
-    print(data.shape)
-    print(data[0, 0])
+    data, features = vectorize_and_transform(files)
 
-    document_indices = above_threshold[0]
-    keyword_indices = above_threshold[1]
+    above_threshold = apply_threshold(data, 0.3)
 
-    feature_names = vectorizer.get_feature_names()
-    print('Features: ', feature_names)
+    document_indices = above_threshold.row
+    keyword_indices = above_threshold.col
 
-    print('Keywords:')
+    print('\n\nAccepted documents: ', len(np.unique(document_indices)))
+
     last_di = -1
     for di, ki in zip(document_indices, keyword_indices):
         if di != last_di:
-            print()
-            print()
-            print(files[di])
+            print('\n\n', files[di])
             last_di = di
-        print(feature_names[ki], end=' | ')
-    print('\n\nAccepted documents: ', len(np.unique(document_indices)))
-    # tensor, headers = evaluate_link_prediction.read_input_tensor(
-    # path + '/rescal')
-    # filemap = {}
-    # index = 1
-    # for line in headers:
-    # if line.startswith('Need: '):
-    #         filemap[line[6:]] = index
-    #     index += 1
-    #
-    #
-    #
-    #     # TODO: map current document indices to new -> filemap[files[di]]
-    #     # TODO: map current feature indices to new -> fi + n_headers
-    #
-    #     # for di, ki in zip(document_indices, keyword_indices):
-    #     # filemap[files[di]]
-
-
-
-
-
+        print(features[ki], end=' | ')
